f1da114c1fcc5f4d76a59522c2ffedcbac97341c
ljhkl <kukakasymov@gmail.com>
2018-01-14 05:23:07 +0300

    Hello

---
+++ Lab1/Prime/Prime/Prime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            double a = 0;
+            string s = Console.ReadLine();
+            string[] massiv = s.Split(' ');
+
+            for (int i = 1; i < massiv.Length; i++)
+            {
+                a = Double.Parse(massiv[i]);
+                for (int j = 1; j <= Math.Sqrt(a); j++)
+                {
+                    if (a % j == 0) k = k + 1;
+                }
+                if (k < 2) Console.WriteLine(a);
+                k = 0;
+            }
+        }
+    }
+}
